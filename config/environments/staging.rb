b7ee28fd48cbea056c424a744c4bc0be7bd86007
Bhagath <bhagath@bhagath-Inspiron-3521.(none)>
2014-05-19 19:56:16 +0530

    Task: added heroku url to environments

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,7 +21,7 @@ WebApp::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-  config.action_mailer.default_url_options = { :host => 'staging-eduposts.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://usccrunchrails.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
